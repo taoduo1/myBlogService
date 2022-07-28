0a79c0c25351d90735da528badebfb5132adc04c
duo.tao <taoduo007wow@outlook.com>
2022-07-28 22:08:07 +0800

    feature(用户服务): 用户登录接口调整 用户登录接口调整

--- shop_project/shop_user/src/main/java/com/example/shop_user/dto/LoginUserDto.java
+++ shop_project/shop_user/src/main/java/com/example/shop_user/dto/LoginUserDto.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Data
 @Accessors(chain = true)
@@ -15,11 +14,10 @@ public class LoginUserDto {
 
     @ApiModelProperty(value = "用户名")
     @NotNull(message = "用户名不能为空")
-    @Size(min = 6,max = 50)
     private String username;
 
     @ApiModelProperty(value = "密码")
-    @Size(min = 6,max = 50)
+    @NotNull(message = "密码不能为空")
     private String password;
 
 
