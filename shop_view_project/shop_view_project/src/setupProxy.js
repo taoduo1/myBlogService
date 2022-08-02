974d01a2754deaa92b862c720b34d94d71d4909f
duo.tao <taoduo007wow@outlook.com>
2022-08-02 22:40:23 +0800

    feature(前端服务): 前端修改端口号 前端修改配置端口号

--- shop_view_project/shop_view_project/src/setupProxy.js
+++ shop_view_project/shop_view_project/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         proxy.createProxyMiddleware({
-            target: 'http://localhost:8081', // 后台服务地址以及端口号
+            target: 'http://localhost:8300', // 后台服务地址以及端口号
             changeOrigin: true, // 是否跨域
             pathRewrite: {
                 '^/api': '' // 路径重写,用/api代替target里的地址
