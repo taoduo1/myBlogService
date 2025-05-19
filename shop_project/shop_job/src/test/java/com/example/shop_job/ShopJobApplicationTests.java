9400fa16fce1ad219ee069aeea72823fd6f46e5b
duo.tao <taoduo007wow@outlook.com>
2025-05-19 21:50:17 +0800

    增加xxljob

---
+++ shop_project/shop_job/src/test/java/com/example/shop_job/ShopJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
